b95d72680ca86d2f3df58770063b137e98c5c709
Ubuntu <ubuntu@ip-172-31-36-63.us-west-2.compute.internal>
2013-07-11 20:29:38 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World2!') ;
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+});[ubuntu@ip-172-31-36-63:~/bitstarter]$
