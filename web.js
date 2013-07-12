c9d23827c58769d4f36b411b48bcbbf7a629655d
videovid <videovid@comcast.net>
2013-07-12 22:28:50 +0000

    Changed to read file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var buffer = Buffer(128);
+
+buffer = fs.readFileSync('Index.html');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!') ;
+  response.send(buf.toString(buffer)) ;
 });
 
 var port = process.env.PORT || 5000;
