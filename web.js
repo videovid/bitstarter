17329cae50efd42674ca6aed920ffa573a129887
videovid <videovid@comcast.net>
2013-07-13 19:28:53 +0000

    Added console command to web.js

--- web.js
+++ web.js
@@ -11,11 +11,12 @@ buffer = fs.readFileSync("./index.html");
 // var app = express.createServer(express.logger());
 var app = express();
 
+console.log("Buffer is: " + buf.toString(buffer));
 app.get('/', function(request, response) {
   response.send(buf.toString(buffer)) ;
 });
 
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
