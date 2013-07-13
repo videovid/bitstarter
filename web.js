68509a455c9d0638fd393830da89b0d671d994c0
videovid <videovid@comcast.net>
2013-07-13 05:39:25 +0000

    took out listener

--- web.js
+++ web.js
@@ -15,7 +15,7 @@ app.get('/', function(request, response) {
   response.send(buf.toString(buffer)) ;
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
