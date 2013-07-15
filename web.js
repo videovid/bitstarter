6da5af5ed5343836edce61d9ef76271391d7f8ce
videovid <videovid@comcast.net>
2013-07-15 23:37:54 +0000

    Trying to redploy with changes

--- web.js
+++ web.js
@@ -1,13 +1,22 @@
-var express = require('express');
+#!/usr/bin/env node
 
-var app = express.createServer(express.logger());
+var fs = require("fs");
 
+var express = require("express");
+
+var buffer = new Buffer(128);
+
+// buffer = fs.readFileSync("./index.html");
+
+   var app = express.createServer(express.logger());
+// var app = express();
+
+// console.log("Buffer is: " + buffer.toString('utf8'));
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs.readFileSync("./index.html").toString()) ;
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
