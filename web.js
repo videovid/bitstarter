740fd3c9e48c0f1f1c5eda2a563def0e8b488b4a
videovid <videovid@comcast.net>
2013-07-15 23:27:38 +0000

    HW1 web.js

--- web.js
+++ web.js
@@ -1,22 +1,13 @@
-#!/usr/bin/env node
+var express = require('express');
 
-var fs = require("fs");
+var app = express.createServer(express.logger());
 
-var express = require("express");
-
-var buffer = new Buffer(128);
-
-// buffer = fs.readFileSync("./index.html");
-
-   var app = express.createServer(express.logger());
-// var app = express();
-
-// console.log("Buffer is: " + buffer.toString('utf8'));
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("./index.html").toString()) ;
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
